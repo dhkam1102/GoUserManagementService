9080ca82a26fff1cee24ea4e3bcdab304b07cee2
Brian Kam <brianhyunkam@gmail.com>
2024-08-06 17:52:08 -0700

    establish connection with db

---
+++ internal/storage/database.go
@@ -0,0 +1,22 @@
+// internal/storage/database.go
+package storage
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func NewDatabaseConnection() *sql.DB {
+	// dsn: Data Source Name
+	dsn := "root:@tcp(127.0.0.1:3306)/user_management"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
